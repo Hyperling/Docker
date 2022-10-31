28ee3b23650e9976ad6e0e0b30cb19bbf24b3aed
Hyperling <me@hyperling.com>
2022-10-30 19:37:15 -0500

    Add DB fixes after upgrading to Nextcloud 25.

--- Config/Nextcloud/fixes.sh
+++ Config/Nextcloud/fixes.sh
@@ -3,7 +3,13 @@
 # Put fixes in a file so they do not need remembered.
 
 docker exec -it nextcloud-app-1 apt update -y
-docker exec -it nextcloud-app-1 apt install -y libmagickcore-6.q16-6-extra
+docker exec -it nextcloud-app-1 apt install -y sudo libmagickcore-6.q16-6-extra
+
+# 2022-10-30 More additions after moving to Nextcloud version 25.
+docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-columns
+docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-indices
+docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-primary-keys
+docker exec -itu www-data nextcloud-app-1 ./occ db:convert-filecache-bigint
 
 # 2022-10-30 More additions after moving to Nextcloud version 25.
 docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-columns
