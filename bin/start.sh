3739e96703ce5dd6bae81791adedc1b6d0e0a036
Hyperling <me@hyperling.com>
2023-07-23 14:10:45 -0700

    Switch to tabs.

--- bin/start.sh
+++ bin/start.sh
@@ -6,20 +6,20 @@
 DIR="`dirname $0`"
 PROG=`basename $0`
 if [[ $DIR == *"."* ]]; then
-  DIR="`pwd`"
+	DIR="`pwd`"
 fi
 
 if [[ -z $DOCKER_HOME ]]; then
-  DOCKER_HOME="$DIR/.."
+	DOCKER_HOME="$DIR/.."
 fi
 
 cd $DOCKER_HOME/Config
 for dir in `ls`; do
-  [ -d $dir ] && cd $dir || continue
-  pwd
-  [ -e Dockerfile ] && docker compose build
-  [ -e docker-compose.yml ] && docker compose up -d
-  cd ..
+	[ -d $dir ] && cd $dir || continue
+	pwd
+	[ -e Dockerfile ] && docker compose build
+	[ -e docker-compose.yml ] && docker compose up -d
+	cd ..
 done
 
 exit 0
