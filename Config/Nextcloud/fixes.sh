c117c6aec7ec6a6e46a1c0f7702a24966d1a693b
Hyperling <me@hyperling.com>
2023-08-19 13:13:30 -0700

    Add ping and dig to Nextcloud container for troubleshooting.

--- Config/Nextcloud/fixes.sh
+++ Config/Nextcloud/fixes.sh
@@ -3,7 +3,7 @@
 # Put fixes in a file so they do not need remembered.
 
 docker exec -it nextcloud-app-1 apt update -y
-docker exec -it nextcloud-app-1 apt install -y sudo libmagickcore-6.q16-6-extra htop
+docker exec -it nextcloud-app-1 apt install -y sudo libmagickcore-6.q16-6-extra htop iputils-ping dnsutils
 
 # 2022-10-30 More additions after moving to Nextcloud version 25.
 docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-columns
