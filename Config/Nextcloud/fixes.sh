27993904118b9b5c3fd7d134812d25585c7b7ca5
Hyperling <me@hyperling.com>
2023-08-20 16:56:32 -0700

    Change container name.

--- Config/Nextcloud/fixes.sh
+++ Config/Nextcloud/fixes.sh
@@ -2,29 +2,35 @@
 # 2022-09-25 Hyperling
 # Put fixes in a file so they do not need remembered.
 
-docker exec -it nextcloud-app-1 apt update -y
-docker exec -it nextcloud-app-1 apt install -y sudo libmagickcore-6.q16-6-extra htop iputils-ping dnsutils
+docker exec -it nextcloud-app apt update -y
+docker exec -it nextcloud-app apt install -y sudo libmagickcore-6.q16-6-extra htop iputils-ping dnsutils
 
 # 2022-10-30 More additions after moving to Nextcloud version 25.
-docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-columns
-docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-indices
-docker exec -itu www-data nextcloud-app-1 ./occ db:add-missing-primary-keys
-docker exec -itu www-data nextcloud-app-1 ./occ db:convert-filecache-bigint
-docker exec -it nextcloud-app-1 chown -Rc www-data:www-data .
+docker exec -itu www-data nextcloud-app ./occ db:add-missing-columns
+docker exec -itu www-data nextcloud-app ./occ db:add-missing-indices
+docker exec -itu www-data nextcloud-app ./occ db:add-missing-primary-keys
+docker exec -itu www-data nextcloud-app ./occ db:convert-filecache-bigint
+docker exec -it nextcloud-app chown -Rc www-data:www-data .
 
 # 2023-02-12 Just for good measure.
-docker exec -itu www-data nextcloud-app-1 ./occ app:update --all
+docker exec -itu www-data nextcloud-app ./occ app:update --all
 
 # 2023-07-02
 # This maybe used to exist, but make sure that Files app is correct.
-docker exec -itu www-data nextcloud-app-1 ./occ files:scan --all
+docker exec -itu www-data nextcloud-app ./occ files:scan --all
 # This one takes a while.
-docker exec -itu www-data nextcloud-app-1 ./occ files:scan-app-data
+docker exec -itu www-data nextcloud-app ./occ files:scan-app-data
 # Extras? Have used the commands in the past and may help in the future.
-docker exec -itu www-data nextcloud-app-1 ./occ maintenance:theme:update
-docker exec -itu www-data nextcloud-app-1 ./occ maintenance:repair
+docker exec -itu www-data nextcloud-app ./occ maintenance:theme:update
+docker exec -itu www-data nextcloud-app ./occ maintenance:repair
 # May alsp be useful but do not have much experience with them.
-docker exec -itu www-data nextcloud-app-1 ./occ versions:cleanup
-docker exec -itu www-data nextcloud-app-1 ./occ files:cleanup
+docker exec -itu www-data nextcloud-app ./occ versions:cleanup
+docker exec -itu www-data nextcloud-app ./occ files:cleanup
+
+# 2023-08-20 For OnlyOffice based on:
+#    https://github.com/ONLYOFFICE/docker-onlyoffice-nextcloud/blob/master/set_configuration.sh
+docker exec -u www-data nextcloud-app php occ --no-warnings app:install onlyoffice
+docker exec -u www-data nextcloud-app php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://nextcloud-oo/"
+docker exec -u www-data nextcloud-app php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nextcloud-app/"
 
 exit 0
