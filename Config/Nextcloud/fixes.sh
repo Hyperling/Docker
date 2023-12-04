33302b9f08fd201bc61e0e5621f63db6d4553622
Hyperling <me@hyperling.com>
2023-12-04 10:52:34 -0700

    Call cron script and enhance output.

--- Config/Nextcloud/fixes.sh
+++ Config/Nextcloud/fixes.sh
@@ -2,16 +2,32 @@
 # 2022-09-25 Hyperling
 # Put fixes in a file so they do not need remembered.
 
+## Setup ##
+
+DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+PROG="$(basename -- "${BASH_SOURCE[0]}")"
+source $DIR/../source.env
+
+## Main ##
+
+echo -e "\n*** APT ***\n"
 docker exec -it nc-app apt update -y
-docker exec -it nc-app apt install -y sudo libmagickcore-6.q16-6-extra htop iputils-ping dnsutils vim
+docker exec -it nc-app apt install -y sudo libmagickcore-6.q16-6-extra htop \
+	iputils-ping dnsutils vim
+
+# 2023-12-04 Make sure cron and chmod commands get run.
+echo -e "\n*** CRON ***\n"
+$DOCKER_HOME/Config/Nextcloud/cron.sh && echo "Success!"
 
 # 2022-10-30 More additions after moving to Nextcloud version 25.
+echo -e "\n*** DATABASE ***\n"
 docker exec -itu www-data nc-app ./occ db:add-missing-columns
 docker exec -itu www-data nc-app ./occ db:add-missing-indices
 docker exec -itu www-data nc-app ./occ db:add-missing-primary-keys
 docker exec -itu www-data nc-app ./occ db:convert-filecache-bigint
 
 # 2023-07-02
+echo -e "\n*** FILES ***\n"
 # This maybe used to exist, but make sure that Files app is correct.
 docker exec -itu www-data nc-app ./occ files:scan --all
 # This one takes a while.
@@ -24,3 +40,4 @@ docker exec -itu www-data nc-app ./occ versions:cleanup
 docker exec -itu www-data nc-app ./occ files:cleanup
 
 exit 0
+
