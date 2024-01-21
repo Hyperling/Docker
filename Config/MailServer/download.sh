49c50b2562f7f8be8cbb0acf47c87fc3088e0dd4
Hyperling <me@hyperling.com>
2024-01-21 13:00:21 -0700

    Abandon unfinished configurations.

--- Config/MailServer/download.sh
+++
@@ -1,5 +0,0 @@
-DMS_GITHUB_URL='https://raw.githubusercontent.com/docker-mailserver/docker-mailserver/master'
-wget "${DMS_GITHUB_URL}/docker-compose.yml"
-wget "${DMS_GITHUB_URL}/mailserver.env"
-wget "${DMS_GITHUB_URL}/setup.sh"
-chmod a+x ./setup.sh
