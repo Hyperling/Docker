2e2480c6037ab05269deb937717beb8a650ce583
Hyperling <me@hyperling.com>
2024-01-24 11:56:06 -0700

    Add the beginnings of a purge script. Helps try to bring down the DB size.

---
+++ Config/Nextcloud/purge.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# 2024-01-24 Hyperling
+# Commands to purge space from both Nextcloud and its database.
+
+## Setup ##
+
+DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+PROG="$(basename -- "${BASH_SOURCE[0]}")"
+source $DIR/../../source.env
+source $DIR/.env
+
+## Main ##
+
+echo -e "\n*** Files ***\n"
+
+echo "`date` - Scanning Files"
+time docker exec -itu www-data nc-app ./occ files:scan --all
+
+echo "`date` - Cleaning Files"
+time docker exec -itu www-data nc-app ./occ files:cleanup
+
+echo -e "\n*** Database ***\n"
+
+echo "`date` - Enabling maintenance mode to avoid any DB issues."
+docker exec -itu www-data nc-app ./occ maintenance:mode --on
+
+# https://mariadb.com/kb/en/mariadb-check/
+
+echo "`date` - Checking DB Tables"
+time docker exec -itu www-data nc-db mariadb-check \
+	-Ac --user="$MYSQL_USER" --password="$MYSQL_PASSWORD"
+
+echo "`date` - Analyzing DB Tables"
+time docker exec -itu www-data nc-db mariadb-check \
+	-Aa --user="$MYSQL_USER" --password="$MYSQL_PASSWORD"
+
+echo "`date` - Optimizing DB Tables -- May take quite some time!!"
+time docker exec -itu www-data nc-db mariadb-check \
+	-Ao --user="$MYSQL_USER" --password="$MYSQL_PASSWORD"
+
+# Purge Spreed Messages?
+
+# Purge File Cache?
+
+# Purge Anything Else?
+
+echo "`date` - Disabling maintenance mode."
+docker exec -itu www-data nc-app ./occ maintenance:mode --off
+
+exit 0
