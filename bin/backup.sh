71da19306d10da26968619220741eed3a87b5bd6
Hyperling <me@hyperling.com>
2024-01-29 19:20:05 -0700

    Add a backup script for the project directory.

---
+++ bin/backup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# 2024-01-29 Hyperling
+# Create a backup file with a generic name for polling.
+
+DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+PROG="$(basename -- "${BASH_SOURCE[0]}")"
+echo "$DIR/$PROG"
+source $DIR/../source.env
+
+## Variables ##
+
+file="/tmp/Backup.zip"
+time="`which time`"
+
+## Main ##
+
+# Remove the last backup.
+if [[ -e $file ]]; then
+	echo "`date` - Removing existing file."
+	rm -fv $file
+fi
+
+echo -e "\n`date` - Take down services for a cold backup."
+manage.sh -d
+
+echo -e "\n`date` - Create the new backup."
+cd $DOCKER_HOME
+pwd
+$time zip -r $file . 1>/dev/null
+
+echo -e "\n`date` - Done with zipping, check size."
+ls -sh $file
+
+echo -e "\n`date` - Ensure other users can access the file."
+chmod -v 755 $file
+
+echo -e "\n`date` - Bring services back up."
+manage.sh -u
+
+## Finish ##
+
+echo -e "\n`date` - Done!"
+exit 0
